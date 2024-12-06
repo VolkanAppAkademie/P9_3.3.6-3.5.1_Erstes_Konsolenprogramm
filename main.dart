05f2fff6671f876c07be6a6cb8c1485c5c8ef441
VolkanAppAkademie <supafly_69@gmx.de>
2024-12-06 11:12:52 +0100

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,13 @@
-void main() {}
+import 'dart:io';
+
+void main() {
+  print('Einnahmen:');
+  double einnahmen = double.parse(stdin.readLineSync()!);
+
+  print('Ausgaben:');
+  double ausgaben = double.parse(stdin.readLineSync()!);
+
+  double bilanz = einnahmen - ausgaben;
+
+  print('Bilanz: \€${bilanz.toStringAsFixed(2)}');
+}
